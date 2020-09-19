1f36e715d25a027d7758fbfa077639fb50d7b98f
khj1030 <69004675+khj1030@users.noreply.github.com>
2020-09-19 17:59:09 +0900

    Add files via upload

---
+++ board/get_post_data.js
@@ -0,0 +1,72 @@
+var temp = [
+    {
+      "post_id": 1,
+      "post_writer": "테스트 작성자",
+      "post_content": "테스트 게시글1",
+      "post_create_at": "2020.09.10",
+      "post_view": 10
+    },
+    {
+      "post_id": 2,
+      "post_writer": "테스트 작성자",
+      "post_content": "테스트 게시글2",
+      "post_create_at": "2020.09.11",
+      "post_view": 35
+    },
+    {
+      "post_id": 3,
+      "post_writer": "테스트 작성자",
+      "post_content": "테스트 게시글3",
+      "post_create_at": "2020.09.12",
+      "post_view": 8
+    },
+    {
+      "post_id": 4,
+      "post_writer": "테스트 작성자",
+      "post_content": "테스트 게시글4",
+      "post_create_at": "2020.09.12",
+      "post_view": 14
+    },
+    {
+      "post_id": 5,
+      "post_writer": "테스트 작성자",
+      "post_content": "테스트 게시글5",
+      "post_create_at": "2020.09.13",
+      "post_view": 5
+    },
+    {
+      "post_id": 6,
+      "post_writer": "테스트 작성자",
+      "post_content": "테스트 게시글6",
+      "post_create_at": "2020.09.13",
+      "post_view": 17
+    },
+    {
+      "post_id": 7,
+      "post_writer": "테스트 작성자",
+      "post_content": "테스트 게시글7",
+      "post_create_at": "2020.09.13",
+      "post_view": 22
+    },
+    {
+      "post_id": 8,
+      "post_writer": "테스트 작성자",
+      "post_content": "테스트 게시글8",
+      "post_create_at": "2020.09.14",
+      "post_view": 20
+    },
+    {
+      "post_id": 9,
+      "post_writer": "테스트 작성자",
+      "post_content": "테스트 게시글9",
+      "post_create_at": "2020.09.14",
+      "post_view": 50
+    },
+    {
+      "post_id": 10,
+      "post_writer": "테스트 작성자",
+      "post_content": "테스트 게시글10",
+      "post_create_at": "2020.09.14",
+      "post_view": 5
+    }
+  ]
